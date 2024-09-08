34065bfaff8636eb7414e50d577e7485ba21eca5
RyukiSasaki <ryuki.sasaki@sasatech.co.jp>
2024-09-08 10:12:37 +0900

    Sを小文字に変更

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,11 +1,11 @@
-import SupabaseClient from "@/utils/supabase/create-client";
+import supabaseClient from "@/utils/supabase/create-client";
 import { NextRequest, NextResponse } from "next/server";
 import { Database } from "@/types/database.types";
 
 type task = Database["public"]["Tables"]["task"]["Insert"]
 
 export async function GET(request: NextRequest) {
-    const supabase = await SupabaseClient()
+    const supabase = await supabaseClient()
     const {data, error} = await supabase.from("task").select("*")
 
     if(error) return NextResponse.json({ error: error.message }, { status: 500 })
@@ -19,9 +19,9 @@ export async function POST(request:NextRequest, response: NextResponse) {
         name: body.name,
     }
 
-    const supabase = await SupabaseClient()
+    const supabase = await supabaseClient()
     const {data, error} = await supabase.from("task").insert(payload).select("id").single()
 
-    if(error) return NextResponse.error
+    if(error) return NextResponse.json({ error: error.message }, { status: 500 })
     return NextResponse.json(data, {status:200})
 }
--- src/utils/supabase/create-client.ts
+++ src/utils/supabase/create-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/database.types'
 
-export default async function SupabaseClient(){
+export default async function supabaseClient(){
     return await createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
 }
